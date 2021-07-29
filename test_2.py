243b782e673aaa3ac91ba523634841ff6c14a987
matheus-roma <matheus.roma@ufpe.br>
2021-07-29 19:34:55 -0300

    test_2

--- test_2.py
+++ test_2.py
@@ -1,7 +1,11 @@
 import cv2
 import utilities as util
 
-img = cv2.imread("images_doc/01.jpeg")
+filename = "images_doc_t2/04.jpeg"
+img = cv2.imread("images_doc/04.jpeg")
+
+#img = cv2.resize(img, (1120, 630))
+
 
 img_Gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_Blur = cv2.GaussianBlur(img_Gray, (25,25), 1)
@@ -9,13 +13,23 @@ img_Canny = cv2.Canny(img_Blur, 100, 100)
 
 
 draw, pts1 = util.getContours(img_Canny, img) 
+#origin = pts1[0]
+
+
+#for i in range(0, 4):
+#        if (pts1[i].any() < origin.any()):
+#            origin = pts1[i]
+
+
 #print(len(pts1))
 #print(pts1)
 
 output = util.adjustImage(img, pts1)
 
-cv2.imshow("Output", output)
-#cv2.imshow("Contours detected",img)
+
+#cv2.imshow("Output", output)
+#cv2.imshow("Contours detected",draw)
 #cv2.imshow("Canny",img_Canny)
 
+cv2.imwrite(filename, output)
 cv2.waitKey(0)
--- utilities.py
+++ utilities.py
@@ -9,16 +9,25 @@ def getContours(canny, img):
         doc_perimeter = cv2.arcLength(i, True)
         if doc_perimeter > 500:
             cv2.drawContours(draw, i, -1, (0,0,255),2)
-            pts = cv2.approxPolyDP(i, 0.05*doc_perimeter, True)
+            pts = cv2.approxPolyDP(i, 0.1*doc_perimeter, True)
+            #print(pts)
             #if len(pts) == 4:
                 #print("The 4 points were detected")
     
     return draw, pts
 
+
 def adjustImage(img, pts):
     x_0, y_0, width, height = cv2.boundingRect(pts)
-    pts2 = np.float64([ [0,0], [width, 0], [0,height], [width, height]])
-    matrix = cv2.getPerspectiveTransform(pts,pts2)
+
+    #print (pts)
+
+    pts = np.float32([ [x_0, y_0], [width, y_0], [x_0,height], [width, height]])
+    #pts = np.float32([ [135, 87], [875, 20], [165,593], [901, 579]])
+    pts2 = np.float32([ [0,0], [width, 0], [0,height], [width, height]])
+    #print(pts.shape, pts2.shape)
+
+    matrix = cv2.getPerspectiveTransform(pts,pts2, solveMethod = cv2.DECOMP_LU)
 
     img_output = cv2.warpPerspective(img, matrix, (width, height))
     
